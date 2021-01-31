e47b161982d1b0e478babda11960cb6bff428ce6
a96lex <a96lex@gmail.com>
2021-01-31 17:38:51 +0100

    cors

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from helpers.database import isInDatabase, getFromDatabase, getExistingTags
 from helpers.youTube import getYoutubeVideos
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/search", methods=["POST"])
