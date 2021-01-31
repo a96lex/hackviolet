fd11e334b2e359f1c51aa40adbb23eb1d81849ac
a96lex <a96lex@gmail.com>
2021-01-31 04:57:12 +0100

    est

--- backend/helpers/database.py
+++ backend/helpers/database.py
@@ -4,14 +4,17 @@ import pymysql
 db_username = os.environ.get("DB_USERNAME")
 db_pass = os.environ.get("DB_PASS")
 db_accepted_videos = os.environ.get("ACCEPTED_VIDEOS")
-db_connexion_name = os.environ.get("CONNEXION_NAME")
+db_connection_name = os.environ.get("CONNECTION_NAME")
 host = os.environ.get("HOST")
 
 
 def isInDatabase(search):
-
+    unix_socket = "/cloudsql/{}".format(db_connection_name)
     cnx = pymysql.connect(
-        user=db_username, password=db_pass, host=host, db=db_accepted_videos
+        user=db_username,
+        password=db_pass,
+        unix_socket=unix_socket,
+        db=db_accepted_videos,
     )
     with cnx.cursor() as cursor:
         cursor.execute("SELECT id FROM * WHERE search = ?", (search,))
